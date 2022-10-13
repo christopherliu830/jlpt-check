5baf075cc8bdcadefc77df4d73c6df123bdc2e2b
Christopher Liu <christopherliu830@gmail.com>
2022-10-13 15:33:13 -0700

    speed up landing

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-
-import { theme } from '../theme/theme';
-
-import { PageContainer } from '../components/PageContainer/PageContainer';
-import { Landing } from '../components/Landing/Landing';
+import { Landing } from '../src/components/Landing/Landing';
 
 export default Landing;
--- web/src/components/Landing/Landing.tsx
+++ web/src/components/Landing/Landing.tsx
@@ -6,49 +6,39 @@ import { FadeInView } from '../FadeInView/FadeInView';
 import { useRouter } from 'next/router';
 
 export function Landing(): React.ReactElement {
-  const [ leaving, setLeaving ] = useState(false);
+  const [leaving, setLeaving] = useState(false);
   const router = useRouter();
 
   const handleStartTest = () => {
     setLeaving(true);
-  }
+  };
 
   const handleAnimationDone = () => {
     if (leaving) router.push('/quiz');
-  }
+  };
 
   return (
-      <Container h="100%" display="flex" flexDirection="column" justifyContent="space-between">
-        <Flex 
-          h="66.66%"
-          direction="column"
-          textAlign="center"
-          alignItems="center"
-          justifyContent="space-around"
-        >
-          <FadeInView open={!leaving}>
-            <Logo fontSize="6xl" />
-            <FadeInView>
-              <Text fontSize="xl" color="gray.600">
-                A diagnostic quiz for Japanese learners.
-              </Text>
-            </FadeInView>
+    <Container h="100%" display="flex" flexDirection="column" justifyContent="space-between">
+      <Flex h="66.66%" direction="column" textAlign="center" alignItems="center" justifyContent="space-around">
+        <FadeInView open={!leaving}>
+          <Logo fontSize="6xl" />
+          <FadeInView>
+            <Text fontSize="xl" color="gray.600">
+              A diagnostic quiz for Japanese learners.
+            </Text>
           </FadeInView>
-          <FadeInView open={!leaving} onAnimationComplete={handleAnimationDone} delay={0.5}>
-            <Button
-              size="lg"
-              margin="24px"
-              onClick={handleStartTest}
-            >
-              Start A Test
-            </Button>
-          </FadeInView>
-        </Flex>
-        <Center margin="24px">
-          <Text color="gray.400" marginTop="auto">
-            Christopher Liu
-          </Text>
-        </Center>
-      </Container>
-  )
+        </FadeInView>
+        <FadeInView open={!leaving} onAnimationComplete={handleAnimationDone} delay={0.2}>
+          <Button size="lg" margin="24px" onClick={handleStartTest}>
+            Start A Test
+          </Button>
+        </FadeInView>
+      </Flex>
+      <Center margin="24px">
+        <Text color="gray.400" marginTop="auto">
+          Christopher Liu
+        </Text>
+      </Center>
+    </Container>
+  );
 }
